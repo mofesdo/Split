249ef6ab471330dac40565509b6bfde01d4ef8e3
Abraham Efraim <75068289+reondaze-a@users.noreply.github.com>
2025-06-06 02:34:44 -0400

    Test page
    created a test-page for js logic

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const typeOfExpenses = ["Food", "Transport", "Accomodation"]
 let payer
 let amount
 
-payer = prompt("Who is paying? (Modesto, Ace, Bradley, Jasmine)")
+// payer = prompt("Who is paying? (Modesto, Ace, Bradley, Jasmine)")
 
-amount = prompt("What is the amount? (in dollars)")
+// amount = prompt("What is the amount? (in dollars)")
+
+
+
 
 console.log(`Payer: ${payer}, Amount: $${amount}`)
