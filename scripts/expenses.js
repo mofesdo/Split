65ab344a383f25e9c01d10e6aaf250d4ce3c4ed6
Modesto <mofesdo.dev@gmail.com>
2025-06-06 04:15:06 -0700

    Feat: Expenses.js now loads expenses from local storage or creates local storage variable

--- scripts/expenses.js
+++ scripts/expenses.js
@@ -1,31 +1,38 @@
-fetch("../expenses.json")
-  .then((res) => res.json())
-  .then((data) => {
-    const expenses = data.expenses;
-    console.log(expenses);
-    console.log(expenses[0]);
-    loadExpenses(expenses);
-  })
-  .catch(console.error);
+// If expenses exist in local storage, load from there. If not, create it from expenses.json
+if (localStorage.getItem("expenses")) {
+  const expenses = JSON.parse(localStorage.getItem("expenses"));
+  loadExpenses(expenses);
+} else {
+  fetch("../expenses.json")
+    .then((res) => res.json())
+    .then((data) => {
+      const expenses = data.expenses;
+      loadExpenses(expenses);
+      localStorage.setItem("expenses", JSON.stringify(expenses));
+    })
+    .catch(console.error);
+}
 
-const loadExpenses = (expenses) => {
+// Reads expenses array and creates a list item for each and appends to unordered list
+function loadExpenses(expenses) {
   const expensesList = document.getElementById("expensesList");
   expenses.forEach((item) => {
     const expenseLi = document.createElement("li");
     const name = document.createElement("p");
-    name.textContent = item.name;
+    name.textContent = "Expense: " + item.name;
     const cost = document.createElement("p");
-    cost.textContent = item.cost;
+    cost.textContent = "Cost: $" + item.cost;
     const payer = document.createElement("p");
-    payer.textContent = item.payer;
-    const friends = document.createElement
-
+    payer.textContent = "Paid By: " + item.payer;
+    const friends = document.createElement("p");
+    friends.textContent = "Owed By: ";
+    item.friends.forEach((friend) => {
+      friends.textContent = friends.textContent + " " + friend + " | ";
+    });
     expenseLi.appendChild(name);
     expenseLi.appendChild(cost);
     expenseLi.appendChild(payer);
+    expenseLi.appendChild(friends);
     expensesList.appendChild(expenseLi);
-    console.log(expenseLi);
   });
-
-  console.log(expensesList);
-};
+}
